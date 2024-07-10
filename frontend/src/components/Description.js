ab80da6c578e9f4dbba396216a2e9f63b18d0201
Thomaz Bonato <thobonato@icloud.com>
2024-07-10 11:26:35 -0400

    update July 10, 2024

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -4,9 +4,9 @@ import resumePDF from '../assets/docs/bonato_thomaz_resume.pdf';
 const Description = () => {
     return (
         <div className="text-l pt-6">
-            <p className="py-2">I'm Thomaz Bonato, an engineer and researcher with a passion for AI/ML. Originally from Brazil 🇧🇷, I've lived across Australia 🇦🇺, Costa Rica 🇨🇷, and the US 🇺🇸. I am currently studying Artificial Intelligence for Business at USC in Los Angeles, where I lead the university's flagship startup incubator, supporting 10+ companies.</p>
-            <p className="py-2">Growing up, I was always involved in math, coding, and robotics. At 13, I created Clevr LMS, a peer tutoring software that sparked my love for technology. My interests extend to AI safety, focusing on using mathematical approaches to develop safer algorithms.</p>
-            <p className="py-2">I love playing tennis, basketball, volleyball and hanging out with friends :)</p>
+            <p className="py-2">👋 Hi! I'm Thomaz -- an engineer, researcher, and AI enthusiast. Originally from Brazil 🇧🇷, I've lived across Australia 🇦🇺, Costa Rica 🇨🇷, and the US 🇺🇸. I am currently studying Artificial Intelligence for Business at USC (Fight On! ✌️) in Los Angeles, where I lead the university's flagship startup incubator, supporting 10+ companies.</p>
+            <p className="py-2">My journey with tech began at 13 when I built Clevr LMS, igniting my passion for coding. Since then, I've fallen in love with hacking away at challenges, working on diverse projects from eco-friendly AI models to NLP to algo trading.</p>
+            <p className="py-2">When I'm not diving into tech, you'll find me playing tennis or basketball, exploring nature, or just chilling with friends :)</p>
             <p className="py-2">See more on my&nbsp;
                     <a className="underline" href={resumePDF} target="_blank" rel="noopener noreferrer">
                         resume
--- frontend/src/components/ProjectsSection.js
+++ frontend/src/components/ProjectsSection.js
@@ -11,7 +11,7 @@ const ProjectsSection = ({ projects }) => {
     };
 
     return (
-        <div className="max-w-6xl mx-auto pt-6 mb-24">
+        <div className="max-w-6xl mx-auto pt-10 sm:pt-6 mb-24">
             <h2 className="text-xl sm:text-2xl font-semibold mb-4 sm:mb-6">projects</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
                 {projects.map((proj) => (
