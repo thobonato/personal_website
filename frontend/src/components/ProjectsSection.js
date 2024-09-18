cf0540a52777749ac4bc13a044478664e1b98802
Thomaz Bonato <thobonato@icloud.com>
2024-09-18 11:59:41 -0700

    update Sep 18, HackMIT win

--- frontend/src/components/ProjectsSection.js
+++ frontend/src/components/ProjectsSection.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Azalea from '../assets/images/azalea.png';
 import Clevr from '../assets/images/clevr.png';
 import Trading from '../assets/images/trading.svg';
-import Citadel from '../assets/images/c1.png';
+import COne from '../assets/images/c1.png';
+import PalmLabs from '../assets/images/palmlabs.png';
+import Mit from '../assets/images/mit.png';
+import Citadel from '../assets/images/citadel.png';
+import Unionville from '../assets/images/unionville.png';
+import Launchology from '../assets/images/launchology.png';
 
 const ProjectsSection = ({ projects }) => {
     const logos = {
         'azalea.png': Azalea,
         'clevr.png': Clevr,
         'trading.svg': Trading,
-        'c1.png': Citadel,
+        'c1.png': COne,
+        'palmlabs.png': PalmLabs,
+    };
+
+    const sponsors = {
+        'mit.png': Mit,
+        'citadel.png': Citadel,
+        'unionville.png': Unionville,
+        'launchology.png': Launchology,
     };
 
     return (
@@ -19,16 +32,21 @@ const ProjectsSection = ({ projects }) => {
                 {projects.map((proj) => (
                     <div key={proj.name} className="bg-white rounded-lg shadow-md overflow-hidden h-auto sm:h-40 flex flex-col transition duration-300 ease-in-out transform hover:scale-[1.03]">
                         <a href={proj.link} target="_blank" rel="noopener noreferrer" className="flex-grow flex flex-col">
-                            <div className="flex items-center p-3 sm:p-4 bg-gray-50 border-b">
+                            <div className="relative flex items-center p-3 sm:p-4 bg-gray-50 border-b shadow-sm">
                                 <img 
                                     src={logos[proj.logo]} 
                                     alt={`${proj.name} logo`} 
                                     className="w-8 h-8 sm:w-10 sm:h-10 object-contain"
                                 />
-                                <div className="">
+                                <div className="flex-grow">
                                     <h3 className="text-base sm:text-lg font-semibold ml-2 sm:ml-3 text-gray-800">{proj.name}</h3>
                                     <h3 className="text-xs sm:text-xs font-regular ml-2 sm:ml-3 text-gray-600">{proj.description}</h3>
                                 </div>
+                                <img
+                                    src={sponsors[proj.sponsor]} 
+                                    alt={`${proj.name} sponsor`} 
+                                    className="absolute top-3 right-3 w-5 h-5 sm:w-6 sm:h-6 object-contain"
+                                />
                             </div>
                             <div className="p-3 sm:p-4 flex items-center justify-center h-full">
                                 <div className="flex items-center justify-between w-full">
