29fc48fe132338886b48eecda48b06d6977ac47c
Thomaz Bonato <bonato@usc.edu>
2025-09-05 12:43:51 -0700

    Fixing build

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'build',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
